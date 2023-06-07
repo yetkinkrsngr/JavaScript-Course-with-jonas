a962088ecb7844b6629259fcaec32ecd21e1f0e3
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-06-07 18:03:01 +0300

    some change

---
+++ 1-Javascritp-Fundamentals-part-1/13-statments-expresion.js
@@ -0,0 +1,26 @@
+// statement: ifade
+//belli başlı bir kod parçasına yani ifadeye denir
+if(false){
+
+}//yani statment olması için bir şeyi ifade etmesi lazım 
+
+
+// expression: değer
+//belli başlı bir değerlere expresion denir
+3+5//expression
+1991//expresion
+true //expresion
+
+//expresion için ifadenin bir değer vermesi lazım.
+
+if(23>10){
+    const str ="23 bigger than 10";
+    //"23 bigger than 10" expresion
+    //const str ="23 bigger than 10"; statment
+}
+
+
+//if(23>10){const str ="23 bigger than 10";} statments
+
+const me="yetkin";
+console.log( `I'm ${1992-2023} years old ${me}`);
---
+++ 1-Javascritp-Fundamentals-part-1/14-trenary.js
@@ -0,0 +1,14 @@
+const age=18;
+age>=18 ? console.log("ı like to drink wine 🍷 "):console.log("under age to drink wine 🍷");
+
+
+// age>=18 // statment
+//? statment if 
+// console.log("ı like to drink wine 🍷 ") if true do this
+// : // else 
+// console.log("under age to drink wine 🍷"); // false do this
+
+console.log(` for driver license ${age>=20 ? console.log("you can get driver licence"): console.log("you can not get driver license") }`);
+
+// küçük alanlara mükemmel bir yapı aslında terk if 
+//else yapısı
