e87fd46ed4f68f34d3dc1ed6a774aef68a064e71
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-06-09 02:47:55 +0300

    ı am trireddd

--- 2-Javascritp-Fundamentals-part-2/4-arrow-function.js
+++ 2-Javascritp-Fundamentals-part-2/4-arrow-function.js
@@ -4,12 +4,13 @@ const calcAge3 = birtYear =>2037-birtYear;
 const age3=calcAge3(1988);
 console.log(age3);
 
-const yearsUntilRetirement = birtYear =>{
+const yearsUntilRetirement = (birtYear,firstName) =>{
     const age=2023-birtYear;
     const retirement=60-age;
-    return retirement;
+    const retirementYears=retirement+2023
+    return `${firstName} retires in ${retirement} years later at ${retirementYears}`;
 }
-console.log(yearsUntilRetirement(1992));
-
+console.log(yearsUntilRetirement(1992,"Yetkin"));
+console.log(yearsUntilRetirement(1988,"Sevim"));
 
 
