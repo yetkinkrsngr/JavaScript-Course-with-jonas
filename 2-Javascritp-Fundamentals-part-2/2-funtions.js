470569ebcc9a2add727d13ccabfb0d4330facc87
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-06-08 20:09:27 +0300

    some change

--- 2-Javascritp-Fundamentals-part-2/2-funtions.js
+++ 2-Javascritp-Fundamentals-part-2/2-funtions.js
@@ -1,1 +1,23 @@
 'use strict';
+/*fonsiyonlar birden fazla kere çalıştırabileceğimiz kod yapısıdır*/
+
+function logger() {
+    console.log("my name is yetkin");
+}
+// calling/running invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples,oranges) {
+    console.log(apples,oranges);
+    const juice=`juice with ${apples} apples and ${oranges} with a oranges.`;
+    return juice;
+}
+const appleJuice=fruitProcessor(5,0);
+console.log(appleJuice);
+
+
+
+const appleOrangeJuice=fruitProcessor(5,4);
+console.log(appleOrangeJuice);
