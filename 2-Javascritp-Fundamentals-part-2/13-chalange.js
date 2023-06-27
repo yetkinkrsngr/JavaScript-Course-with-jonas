3d0c7083dfca43fb2f910e6ab9c97a664b890654
yetkin karasungur <yetkinkrsngr@gmail.com>
2023-06-28 00:55:38 +0300

    some add

---
+++ 2-Javascritp-Fundamentals-part-2/13-chalange.js
@@ -0,0 +1,12 @@
+"use strict";
+let testData1 = [17, 21, 23];
+const temp = function () {
+  for (let i = 0; i < testData1.length; i++) {
+    if (testData1[i] < 20) {
+      console.log(`${testData1[i]}°C in ${i + 1} days.. 😅`);
+    } else {
+      console.log(`${testData1[i]}°C in ${i + 1} days.. 😰`);
+    }
+  }
+};
+temp();
